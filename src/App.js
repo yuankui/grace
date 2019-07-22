908bc5c724309ac9c7a0eca47091f7a069441bb3
yuankui <yuankui@meituan.com>
2019-07-22 14:38:37 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, {Component} from 'react';
 import './App.css';
 import {Editor, EditorState} from 'draft-js';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+    this.onChange = (editorState) => {
+      console.log(editorState);
+      this.setState({editorState});
+    }
   }
   render() {
     return (
+        <div style={{
+          margin: 50,
+          border: '1px solid red',
+        }}>
         <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        </div>
     );
   }
 }
