d99b9a22d66314fd8e410e8847f0b798893514ce
yuankui <yuankui@meituan.com>
2019-07-29 18:02:47 +0800

    add h1

--- src/Editor/plugins/H1/H1.tsx
+++ src/Editor/plugins/H1/H1.tsx
@@ -5,9 +5,26 @@ interface H1Props {
     editable: boolean,
 }
 
-export default class H1 extends BasePlugin<H1Props, object> {
+interface H1State {
+}
+
+interface KeyListener {
+    (key: string): void;
+}
+
+interface ListenerMap {
+    [prop: string]: KeyListener;
+}
+
+export default class H1 extends BasePlugin<H1Props, H1State> {
+
+    keyPress(e: React.KeyboardEvent<HTMLHeadingElement>) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    }
     render() {
-        return <h1 contentEditable={this.props.editable}>
+        return <h1 contentEditable={this.props.editable} onKeyPress={(e) => this.keyPress(e)}>
         </h1>
     }
 };
