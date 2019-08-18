1f898d4d500955f5e36e682cd576f4922170d76c
yuankui <yuankui64@gmail.com>
2019-08-18 22:53:47 +0800

    TODO add image component

---
+++ src/Editor/plugins/image-plugin/index.ts
@@ -0,0 +1,8 @@
+import {EditorState} from "draft-js";
+import {StateChange} from "../../Editor";
+
+export function createImagePlugin(state: EditorState, onChange: StateChange) {
+    return {
+
+    }
+}
