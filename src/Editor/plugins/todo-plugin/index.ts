4e156591a07af8f1094e8f58961893ad55048d42
yuankui <yuankui64@gmail.com>
2019-08-19 23:46:37 +0800

    up

--- src/Editor/plugins/todo-plugin/index.ts
+++ src/Editor/plugins/todo-plugin/index.ts
@@ -1,8 +1,52 @@
 import {GetState, StateChange} from "../../Editor";
-import {ContentBlock} from "draft-js";
+import Draft, {
+    ContentBlock,
+    ContentState,
+    EditorState,
+    DraftHandleValue
+} from "draft-js";
 import {EditorPlugin} from "../index";
 import {TodoBlock} from "./TodoBlock";
 import './index.css';
+
+/*
+Returns default block-level metadata for various block type. Empty object otherwise.
+*/
+const getDefaultBlockData = (blockType: string, initialData = {}) => {
+    if (blockType === 'todo') {
+        return {
+            checked: false,
+        }
+    } else {
+        return initialData
+    }
+};
+
+const resetBlockType = (editorState: EditorState, newType = 'unstyled'): EditorState => {
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+    const key = selectionState.getStartKey();
+    const blockMap = contentState.getBlockMap();
+    const block = blockMap.get(key);
+    let newText = '';
+    const text = block.getText();
+    if (block.getLength() >= 2) {
+        newText = text.substr(1);
+    }
+    const newBlock: ContentBlock = block.merge({
+        text: newText,
+        type: newType,
+        data: getDefaultBlockData(newType),
+    }) as ContentBlock;
+    const newContentState: ContentState = contentState.merge({
+        blockMap: blockMap.set(key, newBlock),
+        selectionAfter: selectionState.merge({
+            anchorOffset: 0,
+            focusOffset: 0,
+        }),
+    }) as ContentState;
+    return EditorState.push(editorState, newContentState, 'change-block-type');
+};
 
 export function createTodoPlugin(getState: GetState, onChange: StateChange): EditorPlugin {
     return {
@@ -23,6 +67,24 @@ export function createTodoPlugin(getState: GetState, onChange: StateChange): Edi
                 return 'block block-todo'
             }
             return null;
+        },
+        handleBeforeInput(str: string, editorState: EditorState, eventTimeStamp: number): DraftHandleValue {
+            if (str !== ']') {
+                return 'not-handled';
+            }
+            /* Get the selection */
+            const selection = editorState.getSelection();
+
+            /* Get the current block */
+            const currentBlock = editorState.getCurrentContent()
+                .getBlockForKey(selection.getStartKey());
+            const blockType = currentBlock.getType();
+            const blockLength = currentBlock.getLength();
+            if (blockLength === 1 && currentBlock.getText() === '[') {
+                onChange(resetBlockType(editorState, blockType !== 'todo' ? 'todo' : 'unstyled'));
+                return 'handled';
+            }
+            return "handled";
         }
     }
 }
