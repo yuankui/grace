6db92cfd28e885ec3569f7f21d5ef3ac4f55fbf0
yuankui <yuankui@meituan.com>
2019-08-23 10:00:24 +0800

    update comment

--- src/Editor/plugins/image-plugin/ImageBlock.tsx
+++ src/Editor/plugins/image-plugin/ImageBlock.tsx
@@ -17,7 +17,7 @@ export interface ImageBlockProps {
 /**
  * TODO: support scale
  * TODO: support caption
- * TODO: save image as a file
+ * TODO: save image as a file: to save space in .git
  */
 export class ImageBlock extends React.Component<ImageProps, any> {
     private readonly content: ReactElement;
