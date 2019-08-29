513ad2c3b5b9fe1758fd9fa526d632b722fe184a
yuankui <yuankui@meituan.com>
2019-08-29 19:18:55 +0800

    add some test

---
+++ src/immutable_test.tsx
@@ -0,0 +1,23 @@
+import {connect} from "react-redux";
+import {AppState} from "./redux/state";
+import React from "react";
+
+function mapState(state: AppState): Props {
+    return {
+        age: 2
+    };
+}
+
+interface Props {
+    age: number,
+}
+
+class Com extends React.Component<Props, any> {
+    render(): any{
+        return <div>hello</div>
+    }
+}
+let Editor = connect(mapState)(Com);
+function Hello() {
+    return <Editor />
+}
