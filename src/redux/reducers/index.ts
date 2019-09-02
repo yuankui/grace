ac5f002294b114a5133e15d2f483230d0943307d
yuankui <yuankui@meituan.com>
2019-09-02 18:56:26 +0800

    redux mei zizi

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,36 +1,19 @@
 import {Post} from "../../backend";
-import {
-    BaseAction,
-    OpeningAction,
-    UpdateEditingPostAction,
-    UpdateEditorStateAction,
-    UpdateListAction
-} from "../actions";
+import {BaseAction, OpeningAction, UpdateEditingPostAction, UpdateListAction} from "../actions";
 import {combineReducers, Reducer} from "redux";
 import {AppStore, createEmptyEditingPost, EditingPost} from "../store";
 import {createNewPostReducer} from "./post_reducers";
 import {OrderedMap} from "immutable";
-import {EditorState} from "draft-js";
 
 
 export function updateEditingPostReducer(post: EditingPost | undefined, action: UpdateEditingPostAction): EditingPost {
-    if (action.type === "OpenPost") {
-        return action.post;
+    if (action.type === "UpdateEditingPost") {
+        return action.editingPost;
     }
     if (post === undefined) {
         return createEmptyEditingPost();
     }
     return post;
-}
-
-export function updateEditorStateReducer(state: EditorState | undefined, action: UpdateEditorStateAction): EditorState {
-    if (action.type === 'UpdateEditorState') {
-        return action.state;
-    }
-    if (state === undefined) {
-        return EditorState.createEmpty();
-    }
-    return state;
 }
 
 export function updatePostList(posts: OrderedMap<string, Post> | undefined = OrderedMap(), action: UpdateListAction): OrderedMap<string, Post> {
@@ -46,6 +29,7 @@ export function updateIsOpening(isOpening: boolean | undefined = false, action: 
     }
     return isOpening as boolean;
 }
+
 
 function concatReducers<S, A extends BaseAction>(reducers: Array<Reducer<S, A>>): Reducer<S, A> {
     return function (state: S | undefined, action: A): S {
